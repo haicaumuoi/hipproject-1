9d17c550c9c7b42dd4b4cb799ef27909383909e1
haicaumuoi <haianh12122002@gmail.com>
2022-10-28 21:17:55 +0700

    Update fetchProject.ts

--- src/utils/functions/fetchProject.ts
+++ src/utils/functions/fetchProject.ts
@@ -1,7 +1,7 @@
 import { Project } from '../../../typing.d';
 
 export const fetchProject = async () => {
-  const res = await fetch(`http://localhost:5000/api/prj/getallprj`);
+  const res = await fetch(`https://hipproback.herokuapp.com/api/prj/getallprj`);
 
   const data = await res.json();
   const projects: Project[] = data.project;
